5bf047cc21c91f7784c54ed5a55e15eb7b7e3fac
Anthony <Anthony_dsant@outlook.com>
2024-04-20 10:58:48 -0300

    feat: creating repository interfaces

---
+++ src/StarLs.Core/Repositories/Interfaces/IMovieRepository.cs
@@ -0,0 +1,10 @@
+﻿using StarLs.Core.Entities;
+
+namespace StarLs.Core.Repositories.Interfaces
+{
+    public interface IMovieRepository
+    {
+        public Task<IEnumerable<Movie>> GetAsync();
+        public Task<Movie> GetByIdAsync(short id);
+    }
+}
---
+++ src/StarLs.Core/Repositories/Interfaces/IPlanetRepository.cs
@@ -0,0 +1,10 @@
+﻿using StarLs.Core.Entities;
+
+namespace StarLs.Core.Repositories.Interfaces
+{
+    public interface IPlanetRepository
+    {
+        public Task<IEnumerable<Planet>> GetAsync();
+        public Task<Planet> GetByIdAsync(short id);
+    }
+}
