a4eaf707752a2e34f9b89fa0ec23949b0c3198ef
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-04-20 14:33:16 -0300

    feat: update Character entitie. Create planetId

--- src/StarLs.Core/Entities/Character.cs
+++ src/StarLs.Core/Entities/Character.cs
@@ -10,6 +10,7 @@
         public string EyeColor { get; private set; } = null !;
         public string BirthYear { get; private set; } = null !;
         public string Gender { get; private set; } = null !;
+        public short PlanetId { get; set; }
         public Planet Planet { get; private set; } = null !;
         public List<Movie> Movies { get; private set; } = null !;
     }
