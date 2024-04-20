e8e57a71eaa024015cc634dbbf06a85394f72f53
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-04-20 13:38:26 -0300

    feat: configuration for migration

---
+++ src/StarLs.Api/Extensions/Configurations.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarLs.Infra.Context;
+
+namespace StarLs.Api.Extensions;
+
+public static class Configurations
+{
+    public static void ConfigureDatabase(this WebApplicationBuilder builder)
+    {
+        var connString = "DataSource=app.db;Cache=Shared";
+
+        builder.Services.AddDbContext<AppDbContext>(x =>
+        {
+            x.UseSqlite(connString, b => b.MigrationsAssembly("StarLs.Api"));
+        });
+    }
+}
--- src/StarLs.Api/Program.cs
+++ src/StarLs.Api/Program.cs
@@ -1,4 +1,8 @@
+using StarLs.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.ConfigureDatabase();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
