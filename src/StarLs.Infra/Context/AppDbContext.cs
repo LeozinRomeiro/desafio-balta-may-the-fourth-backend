1353b2ee128bf3f376aeae7e5f4d2d0dde1f7de4
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-04-20 11:05:21 -0300

    feat: create dbContext

---
+++ src/StarLs.Infra/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StarLs.Core.Entities;
+
+namespace StarLs.Infra.Context;
+public class AppDbContext : DbContext
+{
+    public DbSet<Movie> Movies { get; private set; }
+    public DbSet<Planet> Planets { get; private set; }
+    public DbSet<Starship> Starships { get; private set; }
+    public DbSet<Vehicle> Vehicles { get; private set; }
+    public DbSet<Character> Characters { get; private set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+    {   
+    }
+
+}
