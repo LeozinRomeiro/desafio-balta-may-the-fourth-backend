6ada74516983ad7ef79f445ef545787a9c55739b
Anthony <Anthony_dsant@outlook.com>
2024-04-20 10:47:37 -0300

    feat: creating entities

---
+++ src/StarLs.Core/Entities/Starship.cs
@@ -0,0 +1,20 @@
+﻿namespace StarLs.Core.Entities
+{
+    public class Starship : Entity
+    {
+        public string Name { get; private set; } = null !;
+        public string Model { get; private set; } = null !;
+        public string Manufacturer { get; private set; } = null !;
+        public string CostInCredits { get; private set; } = null !;
+        public string Length { get; private set; } = null !;
+        public string MaxSpeed { get; private set; } = null !;
+        public string Crew { get; private set; } = null !;
+        public string Passengers { get; private set; } = null !;
+        public string CargoCapacity { get; private set; } = null !;
+        public string HyperdriveRating { get; private set; } = null !;
+        public string Mglt { get; private set; } = null !;
+        public string Consumables { get; private set; } = null !;
+        public string Class { get; private set; } = null !;
+        public List<Movie> Movies { get; private set; } = null !;
+    }
+}
