cc555449ccb077b4b919d4bc484483940a6bcf2b
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-04-26 22:36:30 -0300

    feat: fix database path

--- src/StarLs.SeedDataBase/Databse/SqliteDbConnection.cs
+++ src/StarLs.SeedDataBase/Databse/SqliteDbConnection.cs
@@ -1,9 +1,10 @@
 ﻿using System.Data.SQLite;
+using System.IO;
 
 namespace StarLs.SeedDataBase.Databse;
 internal class SqliteDbConnection : IDbConnection<SQLiteConnection>, IDisposable
 {
-    public readonly string _connStr = @"DataSource=C:\csharp\Balta\desafio-balta-may-the-fourth-backend\src\StarLs.Api\app.db;version=3;";
+    public readonly string _connStr = "";
     private SQLiteConnection _conn;
 
     public void Dispose()
@@ -13,6 +14,10 @@ internal class SqliteDbConnection : IDbConnection<SQLiteConnection>, IDisposable
 
     public SQLiteConnection GetConnection()
     {
+        var currentDic = Directory.GetCurrentDirectory();
+        var pathDatabase = Directory.GetParent(Directory.GetParent(Directory.GetParent(Directory.GetParent(currentDic).ToString()).ToString()).ToString());
+        var _connStr = $"DataSource={Path.Combine(pathDatabase.FullName, "StarLs.Api", "app.db")},version=3;";
+
         _conn = new SQLiteConnection(_connStr);
         _conn.Open();
 
