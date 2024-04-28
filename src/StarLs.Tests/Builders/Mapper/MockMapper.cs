886d08c062e0eafbe59bd2436ce2567c77258113
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-04-28 16:55:04 -0300

    feat: mock builders

---
+++ src/StarLs.Tests/Builders/Mapper/MockMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using StarLs.Application.Mappings;
+
+namespace StarLs.Tests.Builders.Mapper;
+public class MockMapper
+{
+    private static IMapper _mapper = null!;
+
+    public static IMapper Build()
+    {
+        MapperConfiguration confMapper = new MapperConfiguration(conf =>
+        {
+            conf.AddProfile(new AutoMapperProfile());
+        });
+
+        _mapper = confMapper.CreateMapper();
+
+        return _mapper;
+    }
+}
